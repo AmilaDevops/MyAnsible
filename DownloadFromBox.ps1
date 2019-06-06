13be5851e58c94554e75ab75d8040bb6d3db2591
Amila Devops <amilag.solid@gmail.com>
2019-06-06 06:36:21 -0700

    7 th commit

---
+++ DownloadFromBox.ps1
@@ -0,0 +1,37 @@
+#!/usr/bin/env powershell
+
+$ErrorActionPreference = "Stop"
+$CURRENTPATH=$pwd.Path
+$pipeline= 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try {
+		if($args[0] -eq 'masier')
+		
